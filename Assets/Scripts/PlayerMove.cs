3ac1ac46c4083a00019c4f490645f1f3d53c8832
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2021-10-15 14:35:10 +0900

    操作ミスによりリポジトリといくつかの変更が失われました　以下復帰作業　Cinemachine Progrids再インポート　バーチャルカメラの設定復元　マンデルブロ集合のシェーダーとマテリアルの作成、再アタッチ　復元作業継続

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed = 4.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
