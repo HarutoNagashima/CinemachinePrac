36696319463488a94cf4ca9773036cd87487bb7d
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2021-10-18 02:08:57 +0900

    画面全体の色調補正ができるようになりました　赤色が薄くなる補正を入れました　新たなモデルとそれ用のいくつかのモーションをインポートしました　移動と入力を受け取るコードを分離しディレクターを配置しました　移動ができなくなりました

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,48 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    // 移動速度
-    [SerializeField]
-    private float moveSpeed = 2.0f;
-    // 移動量
-    private Vector3 mov = Vector3.zero;
-
-    void FixedUpdate()
-    {
-        // 移動させる
-        transform.position += mov;
-    }
-
-    public void playerMove(InputAction.CallbackContext context)
+    protected Vector2 gamePad;
+    public void CatchInput(InputAction.CallbackContext context)
     {
         // ゲームパッドの入力取得
-        var gamePad = context.ReadValue<Vector2>();
-        // ゲームパッドのNullチェック
-        if (Gamepad.current == null) return;
-        // カメラの方向からX-Z平面の単位ベクトルを取得
-        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        // 移動量の計算
-        mov = cameraForward*gamePad.y*moveSpeed*Time.deltaTime
-            +Camera.main.transform.right*gamePad.x*moveSpeed*Time.deltaTime;
-
-        switch (context.phase)
-        {
-            // 入力開始
-            case InputActionPhase.Started:
-                transform.forward = mov; // 倒した方向に向く
-                break;
-
-            case InputActionPhase.Canceled: // 入力終了
-                break;
-
-            default:
-                transform.forward = mov;
-                break;
-        }
-
+        gamePad = context.ReadValue<Vector2>();
+        Debug.Log(gamePad);
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : PlayerInput
 {
-
+    // 移動速度
     [SerializeField]
-    private float moveSpeed = 4.0f;
-    void Start()
+    private float moveSpeed = 2.0f;
+    // 移動量
+    private Vector3 mov = Vector3.zero;
+
+    public void playerMove(InputAction.CallbackContext context)
     {
-        
+        // ゲームパッドのNullチェック
+        if (Gamepad.current == null) return;
+        // カメラの方向からX-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        // 移動量の計算
+        mov = cameraForward * gamePad.y * moveSpeed * Time.deltaTime
+            + Camera.main.transform.right * gamePad.x * moveSpeed * Time.deltaTime;
+
+        /*switch (context.phase)
+        {
+            // 入力開始
+            case InputActionPhase.Started:
+                transform.forward = mov; // 倒した方向に向く
+                break;
+
+            case InputActionPhase.Canceled: // 入力終了
+                break;
+
+            default:
+                transform.forward = mov;
+                break;
+        }*/
+        transform.forward = mov;
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        // 移動させる
+        this.transform.position += mov;
     }
+
 }
