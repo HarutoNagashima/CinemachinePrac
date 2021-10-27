95f6de0d5ca5c04293e777f8000e0d25ac93be9c
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2021-10-27 13:33:10 +0900

    モーションデータと移動モーションのブレンドツリー作成

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,12 @@ public class PlayerMove : MonoBehaviour
     private Vector3 mov = Vector3.zero;
     // ゲームパッドの変数
     private Vector2 gamePad;
+    // アニメーターの参照
+    [SerializeField]
+    private Animator anim;
+    // プレイヤーのリジッドボディ参照
+    [SerializeField]
+    private Rigidbody rig;
     /*// 入力を受け取るスクリプトの参照
     private PlayerInputCatch playerInput;*/
 
@@ -22,8 +28,7 @@ public class PlayerMove : MonoBehaviour
         // カメラの方向からX-Z平面の単位ベクトルを取得
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
         // 移動量の計算
-        mov = cameraForward * gamePad.y * moveSpeed * Time.deltaTime
-            + Camera.main.transform.right * gamePad.x * moveSpeed * Time.deltaTime;
+        mov = (cameraForward * gamePad.y * moveSpeed + Camera.main.transform.right * gamePad.x * moveSpeed)*Time.deltaTime;
 
         switch (context.phase)
         {
@@ -41,10 +46,19 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    void ApplyAnimatorParameter()
+    {
+        var speed = Mathf.Abs(gamePad.x);
+        Debug.Log(speed);
+        anim.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+    }
+
     void FixedUpdate()
     {
         // 移動させる
         transform.position += mov;
+        // アニメーター設定
+        ApplyAnimatorParameter();
     }
 
 }
